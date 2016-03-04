2c7c09c53ff5905bbc60067de8368a2e080f5aef
Jenna Kwon <jinkwon06@gmail.com>
2016-03-04 14:22:48 -0500

    graph and map visualization added

--- client/views/home_private/home_private.js
+++ client/views/home_private/home_private.js
@@ -1,5 +1,10 @@
 Template.HomePrivate.rendered = function() {
-	
+    //$.getScript("/d3.js");
+    //$.getScript("/crossfilter.js");
+    //$.getScript("/dc.js");
+    //$.getScript("/colorbrewer.js");
+
+    $.getScript("/filters.js");
 };
 
 Template.HomePrivate.events({
